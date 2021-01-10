c41ffec2508737e36bc4fa4ea65c3958992b1a84
Matahari K Herwin <matahari@nyu.edu>
2021-01-10 19:16:30 +0700

    added drink and fixed errors

--- gofood.rb
+++ gofood.rb
@@ -1,6 +1,7 @@
 class Food
-  attr_reader :name
-  def initialize(name, price, vegan, tags, healthy, origin)
+  attr_reader :name, :price, :tags
+
+  def init(name, price, vegan, tags, healthy, origin)
     @name = name
     @price = price
     @vegan = vegan
@@ -10,39 +11,100 @@ class Food
   end
 
   def to_s
-    "#{@name} Rp #{@price} #{@vegan} tags: #{@tags.join(', ')} #{@healthy} #{@origin}"
+    "#{@name} Rp #{@price} #{@vegan} tags:#{@tags.join(', ')}, #{@healthy}, #{@origin}"
   end
 end
 
 class Drink
-  attr_reader :name
-  def initialize(name, price, size, hot, caffeine, tags, origin)
+  attr_reader :name, :price, :tags
+
+  def init(name, price, size, temp, caffeine, tags, origin)
     @name = name
     @price = price
     @size = size
-    @hot = hot
+    @temp = temp
     @caffeine = caffeine
     @tags = tags
     @origin = origin
   end
 
   def to_s
-    "#{@name} Rp #{@price} #{@size} #{@hot} #{@caffeine} tags: #{@tags.join(', ')} #{@origin}"
+    "#{@name} Rp #{@price} #{@size} #{@temp} #{@caffeine} tags:#{@tags.join(', ')}, #{@origin}"
   end
 end
 
-puts "Welcome to Go-Food \nPlease enter one of the following commands: \ncreate_menu \nadd_food \nadd_drink \ngive_recommendations"
+class Recommender
+  attr_reader :menu
 
-begin
-  input = gets.chomp
-  command, *args = input.split(" ")
-  preference = {}
-  recommendations = {}
-  
-  if command == 'create_menu'
-    puts "Created #{args[0]} menu"
-    menu = []
-  elsif command == 'add_food'
+  def initialize
+    @menu = []
+    @preference = {}
+    @recommendations = []
+  end
+
+  def prompt_preference
+    puts "Do you want to drink or eat?"
+    @preference[:type] = gets.chomp
+
+    puts "What flavor do you prefer?"
+    @preference[:flavor] = gets.chomp
+
+    puts "How much budget do you have?"
+    budget = gets.chomp
+    min_budget, max_budget = budget.split("-")
+    @preference[:min_budget] = min_budget.to_i
+    @preference[:max_budget] = max_budget.to_i
+  end  
+
+  def recommend
+    for item in @menu
+      case @preference[:type]
+      when "eat"
+        @recommendations << item if item.tags.include?(@preference[:flavor]) && (@preference[:min_budget] <= item.price && @preference[:max_budget] >= item.price)
+      end
+    end
+  end
+
+  def print_recommendation
+    if @recommendations == nil
+      puts "Sorry we don't have any recommendations for you"
+    else
+      puts "Recommendations for you:"
+      @recommendations.each do |recommendation|
+        puts recommendation.name
+      end
+    end
+  end
+end
+
+class CommandParser
+  def init
+    @recommender = Recommender.new
+  end
+
+  def run
+    puts "Welcome to GoFood"
+
+    begin
+      input = gets.chomp
+      command, *args = input.split(" ")
+
+      case command
+      when 'create_menu'
+        puts "created #{args[0]} menu"
+      when 'add_food'
+        execute_add_food(args)
+      when 'add_drink'
+        execute_add_drink(args)
+      when 'list_menu'
+        execute_list_menu
+      when 'give_recommendations'
+        execute_give_recommendations
+      end
+    end while command != 'exit'
+  end
+
+  def execute_add_food(args)
     name = args[0]
     price = args[1].to_i
     vegan = args[2]
@@ -51,60 +113,37 @@ begin
     origin = args[-1]
 
     food = Food.new(name, price, vegan, tags, healthy, origin)
-    menu << food
-    puts "#{food.name} added"
+    @recommender.menu << food
 
-  elsif command == 'add_drink'
+    puts "#{food.name} added"  
+  end
+
+  def execute_add_drink(args)
     name = args[0]
     price = args[1].to_i
     size = args[2]
-    hot = args[3]
+    temp = args[3]
     caffeine = args[4]
     tags = args[5..-2]
     origin = args[-1]
 
-    drink = Drink.new(name, price, size, hot, caffeine, tags, origin)
-    menu << drink
+    drink = Drink.new(name, price, size, temp, caffeine, tags, origin)
+    @recommender.menu << drink
     puts "#{drink.name} added"
-
-  elsif command == 'list_menu'
-    for item in menu.each
+  end
+  
+  def execute_list_menu
+    for item in @recommender.menu
       puts item
     end
-
-  elsif command == 'give_recommendations'
-    puts "Do you want to drink or eat?"
-    preference[:type] = gets.chomp
-
-    puts "What flavor do you prefer?"
-    preference[:flavor] = gets.chomp
-
-    puts "How much budget do you have?"
-    budget = gets.chomp
-    min_budget, max_budget = budget.split("-")
-    preference[:min_budget] = min_budget.to_i
-    preference[:max_budget] = max_budget.to_i
-
-    for item in menu
-      case preference[:type]
-      when "eat"
-        puts item.tags.include?(preference[:flavor])
-        puts preference[:min_budget] <= item.price
-        puts preference[:max_budget] >= item.price
-        recommendations << item if item.tags.include?(preference[:flavor]) && (preference[:min_budget] <= item.price && preference[:max_budget] >= item.price)
-      end
-    end
-
-    if recommendations == nil
-      puts "Sorry, we don't have anything to recommend to you."
-    else
-      puts "Recommendations for you:"
-      recommendations.each do [recommendation]
-        puts recommendation.name
-      end
-    end
-
-  else puts "Invalid command. Please try again."
   end
 
-end while command != 'exit'
+  def execute_give_recommendations
+    @recommender.prompt_preference
+    @recommender.recommend
+    @recommender.print_recommendation
+  end
+end
+
+command_parser = CommandParser.new
+command_parser.run
